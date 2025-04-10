d9adb39e8040a61adf4e7e92bd8a9d7614773991
nikhilkalburgi <nikhilkalburgi19@gmail.com>
2025-04-10 21:23:43 +0530

    Add gunicorn, dj-database-url

--- finance_tracker_project/settings.py
+++ finance_tracker_project/settings.py
@@ -1,6 +1,7 @@
 import os
 from datetime import timedelta
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,6 +132,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
